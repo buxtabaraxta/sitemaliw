05510bf2bce4a6c7011861cc79b0d937a6f169b2
buxta_baraxta <buxta_baraxta@yahoo.com>
2019-10-01 00:14:53 +0300

    Created index gallery and item pages

---
+++ app/routes.py
@@ -0,0 +1,41 @@
+from app import app
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'David'}
+    posts = [
+        {
+            'author': {'username': 'Joshua'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Kozi'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title="Home page", user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('index')
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/element', methods=['GET'])
+def element():
+
+    return render_template('element.html', title='Element')\
+
+
+@app.route('/gallery', methods=['GET'])
+def gallery():
+    images = [x for x in range(0,10)]
+    print(images)
+    return render_template('gallery.html', title='Gallery', images=images)
